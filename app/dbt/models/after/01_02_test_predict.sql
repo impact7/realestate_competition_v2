49f8d7eb1867ccecbbffa0f4d570b2256582235f
hideki <impact7@i.softbank.jp>
2026-01-09 19:01:13 +0900

    update

---
+++ app/dbt/models/after/01_02_test_predict.sql
@@ -0,0 +1,15 @@
+select
+    lpad(cast(t1.id as text), 6, '0') as id,
+    cast((t2.y_pred * t1.house_area + t3.y_pred)/2 as bigint) as predict
+from
+    "prepare_data"."04_15_test_dm" as t1
+    inner join
+    "output"."test_predict" as t2
+    on
+        t1.id = t2.id
+    inner join
+    "output"."test_predict2" as t3
+    on
+        t1.id = t3.id
+order by
+    t1.id
